cf34ca23b5b8a4fa7517a0053465a5f231cff584
Sudhirnandhan <contactsudhirnandhan@gmail.com>
2026-02-04 12:15:51 +0530

    Configure dynamic port for cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -48,11 +48,11 @@ const connectDB = async () => {
 
 connectDB();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT,
-    console.log(`Server running on port ${PORT} (Using JSON File DB)`)
+    console.log(`Server running on port ${PORT}`)
 );
 
 // Handle unhandled promise rejections
